7c14601ed7493d81b5d6505036106f0d8d8b410b
nasuf <nasuf@DESKTOP-F0I7644>
2021-12-08 07:13:34 +0300

    Add project files.

---
+++ DataAccessLayer/Migrations/202112051038582_0512021338.cs
@@ -0,0 +1,110 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class _0512021338 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Abouts",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        AboutDetail1 = c.String(maxLength: 100),
+                        AboutDetail2 = c.String(maxLength: 100),
+                        AboutImage1 = c.String(maxLength: 100),
+                        AboutImage2 = c.String(maxLength: 100),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        CategoryName = c.String(maxLength: 50),
+                        CategoryDescription = c.String(maxLength: 200),
+                        CategoryStatus = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.Headings",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        HeadingName = c.String(maxLength: 50),
+                        HeadingDate = c.DateTime(nullable: false),
+                        CategoryID = c.Int(nullable: false),
+                        WriterID = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.Categories", t => t.CategoryID, cascadeDelete: true)
+                .ForeignKey("dbo.Writers", t => t.WriterID, cascadeDelete: true)
+                .Index(t => t.CategoryID)
+                .Index(t => t.WriterID);
+            
+            CreateTable(
+                "dbo.Contents",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        ContentValue = c.String(maxLength: 100),
+                        ContentDate = c.DateTime(nullable: false),
+                        HeadingID = c.Int(nullable: false),
+                        WriterID = c.Int(),
+                    })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.Headings", t => t.HeadingID, cascadeDelete: true)
+                .ForeignKey("dbo.Writers", t => t.WriterID)
+                .Index(t => t.HeadingID)
+                .Index(t => t.WriterID);
+            
+            CreateTable(
+                "dbo.Writers",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        WriterName = c.String(maxLength: 50),
+                        WriterSurname = c.String(maxLength: 50),
+                        WriterImage = c.String(maxLength: 100),
+                        WriterMail = c.String(maxLength: 50),
+                        WriterPassword = c.String(maxLength: 20),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.Contacts",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        UserName = c.String(maxLength: 50),
+                        UserMail = c.String(maxLength: 50),
+                        Subject = c.String(maxLength: 50),
+                        Message = c.String(maxLength: 100),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Headings", "WriterID", "dbo.Writers");
+            DropForeignKey("dbo.Contents", "WriterID", "dbo.Writers");
+            DropForeignKey("dbo.Contents", "HeadingID", "dbo.Headings");
+            DropForeignKey("dbo.Headings", "CategoryID", "dbo.Categories");
+            DropIndex("dbo.Contents", new[] { "WriterID" });
+            DropIndex("dbo.Contents", new[] { "HeadingID" });
+            DropIndex("dbo.Headings", new[] { "WriterID" });
+            DropIndex("dbo.Headings", new[] { "CategoryID" });
+            DropTable("dbo.Contacts");
+            DropTable("dbo.Writers");
+            DropTable("dbo.Contents");
+            DropTable("dbo.Headings");
+            DropTable("dbo.Categories");
+            DropTable("dbo.Abouts");
+        }
+    }
+}
